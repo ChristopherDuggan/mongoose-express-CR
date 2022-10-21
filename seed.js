e2ac919b8f21f9cc74effd83ea22d41ebba5f6a5
Christopher Duggan <christopherduggan@gmail.com>
2022-10-21 05:22:58 -0400

    add seed and script

---
+++ seed.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+import connection from './connection.js'
+import List from './models/List.js'
+
+const listData = [{
+    name: "Chris' Todo List,
+    items: [
+        {
+            title: "test db"
+            status: "complete",
+            deadline: "tonight"
+        },
+        {
+            title: "Go to bed",
+            status: "incomplete"
+            deadline: "a while ago"
+        }
+    ]
+}]
+
+List
+    .deleteMany({})
+    .then(() => List.create(listData))
+    .then(() => mongoose.disconnect())
+    .catch(() => error => console.error("seeding error: ", error))
